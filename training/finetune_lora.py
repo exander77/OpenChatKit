7588b88ad7e94a514d8a2162f84e273b276cf1a3
orangetin <orangetin@mail.atomicmail.me>
2023-04-04 17:57:08 -0500

    Add peft training/inference

--- training/finetune_lora.py
+++ training/finetune_lora.py
@@ -20,21 +20,21 @@ model = AutoModelForCausalLM.from_pretrained(
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-for param in model.parameters():
-    param.requires_grad = False  # freeze the model - train adapters later
-    if param.ndim == 1:
-        # cast the small parameters (e.g. layernorm) to fp32 for stability
-        param.data = param.data.to(torch.float32)
-
-model.gradient_checkpointing_enable()  # reduce number of stored activations
-model.enable_input_require_grads()
-
-
-class CastOutputToFloat(nn.Sequential):
-    def forward(self, x): return super().forward(x).to(torch.float32)
-
-
-model.lm_head = CastOutputToFloat(model.lm_head)
+# for param in model.parameters():
+#     param.requires_grad = False  # freeze the model - train adapters later
+#     if param.ndim == 1:
+#         # cast the small parameters (e.g. layernorm) to fp32 for stability
+#         param.data = param.data.to(torch.float32)
+#
+# model.gradient_checkpointing_enable()  # reduce number of stored activations
+# model.enable_input_require_grads()
+#
+#
+# class CastOutputToFloat(nn.Sequential):
+#     def forward(self, x): return super().forward(x).to(torch.float32)
+#
+#
+# model.lm_head = CastOutputToFloat(model.lm_head)
 
 
 def print_trainable_parameters(model):
